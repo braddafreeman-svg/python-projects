20aeb6667cad0450623701bf9684d217ea24693e
Bradda Freeman <bradda.freeman@gmail.com>
2026-02-11 12:49:04 +0000

    Add custom module for message generation

--- 10_time_travel_generator/custom_module.py
+++ 10_time_travel_generator/custom_module.py
@@ -1,1 +1,8 @@
+def generate_time_travel_message(year, destination, cost):
+    return (
+        "Your time travel experience is booked!\n"
+        "Destination: {}\n"
+        "Year: {}\n"
+        "Total Cost: £{}".format(destination, year, cost)
+    )
 
